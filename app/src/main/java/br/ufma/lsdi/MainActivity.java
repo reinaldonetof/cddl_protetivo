6d06c7abe8a377aa610685529a00da11f8dc2525
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-04-25 10:13:49 -0300

    Sub, pub and permission tweaks

--- app/src/main/java/br/ufma/lsdi/MainActivity.java
+++ app/src/main/java/br/ufma/lsdi/MainActivity.java
@@ -4,14 +4,7 @@ import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static android.os.Build.VERSION.SDK_INT;
 
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContract;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -23,6 +16,11 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import br.ufma.lsdi.cddl.CDDL;
 import br.ufma.lsdi.cddl.ConnectionFactory;
@@ -42,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView messageTextView;
     private View sendButton;
     private ConnectionImpl con;
+    private ConnectionImpl external_con;
+    private Double latitude = 0.0;
+    private Double longitude = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +50,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setViews();
 
-        if(!checkPermission()){
-            requestPermission();
-        }
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
+//        if(!checkPermission()){
+//            requestPermission();
+//        }
 
         // Este codigo comentado envolve o uso seguro do CDDL. Estes métodos não devem ser chamados de forma sequencial.
         // Caso alguma equipe deseje usar o CDDL no modo seguro me procure que eu explico melhor (andre.cardoso@lsdi.ufma.br)
@@ -63,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
         initCDDL();
         subscribeMessage();
+        subscribeExternalMessage();
         sendButton.setOnClickListener(clickListener);
     }
 
@@ -70,47 +74,114 @@ public class MainActivity extends AppCompatActivity {
         sendButton = findViewById(R.id.sendButton);
         messageTextView = findViewById(R.id.messageTexView);
     }
+
     private void initCDDL() {
         String host = CDDL.startMicroBroker();
-        //String host = CDDL.startSecureMicroBroker(getApplicationContext(), true);
-        //val host = "broker.hivemq.com";
+        //String host = CDDL.startSecureMicroBroker(getApplicationContext(), true)
         con = ConnectionFactory.createConnection();
-        con.setClientId("andre.cardoso@lsdi.ufma.br2");
+        con.setClientId("clientId-6Uzg69fAi7");
         con.setHost(host);
         con.addConnectionListener(connectionListener);
         con.connect();
+
+        String host_external = "broker.hivemq.com";
+        external_con = ConnectionFactory.createConnection();
+        external_con.setClientId("clientId-6Uzg69fAi7");
+        external_con.setHost(host_external);
+        external_con.addConnectionListener(connectionListenerExternal);
+        external_con.connect();
 //        con.secureConnect(getApplicationContext());
         cddl = CDDL.getInstance();
         cddl.setConnection(con);
         cddl.setContext(this);
         cddl.startService();
+        cddl.startLocationSensor();
         cddl.startCommunicationTechnology(CDDL.INTERNAL_TECHNOLOGY_ID);
-        //cddl.startLocationSensor();
     }
+
     private void publishMessage() {
         Publisher publisher = PublisherFactory.createPublisher();
         publisher.addConnection(cddl.getConnection());
         MyMessage message = new MyMessage();
-        message.setServiceName("Meu serviço");
+        message.setServiceName("my_service");
         message.setServiceByteArray("Valor");
         publisher.publish(message);
     }
-    private void subscribeMessage() {
+
+    private void publishExternal(Double lat, Double lon) {
+        Publisher publisher = PublisherFactory.createPublisher();
+        publisher.addConnection(external_con);
+        MyMessage message = new MyMessage();
+        message.setServiceName("my_service");
+        message.setServiceValue(lat + ";" + lon);
+        publisher.publish(message);
+    }
+
+    private void subscribeExternalMessage() {
         Subscriber sub = SubscriberFactory.createSubscriber();
-        sub.addConnection(cddl.getConnection());
-        sub.subscribeServiceByName("Meu serviço");
-        //sub.subscribeServiceByName("Location");
+        sub.addConnection(external_con);
+        sub.subscribeServiceByName("location_external");
+        sub.subscribeServiceByName("alert");
+        sub.subscribeServiceByName("my_service");
         sub.setSubscriberListener(new ISubscriberListener() {
             @Override
             public void onMessageArrived(Message message) {
-                if (message.getServiceName().equals("Meu serviço")) {
-                    Log.d("_MAIN", "+++" + message);
+                if (message.getServiceName().equals("my_service")) {
+                    Log.d("_MAIN_EXTERNAL_MY_SERVICE", "+++" + message.toString());
+                }
+                if (message.getServiceName().equals("alert")) {
+                    Log.d("_MAIN_EXTERNAL_ALERTA", "+++" + message.toString());
+                }
+                if (message.getServiceName().equals("location_external")) {
+                    Log.d("_MAIN_EXTERNAL_LOCATION", "+++" + message.toString());
+                }
+                Log.d("_MAIN_EXTERNAL", ">>>>>>>>>>>>>>>>>>>>>>>>>>>>" + message);
+            }
+        });
+    }
+
+    private void subscribeMessage() {
+        Subscriber sub = SubscriberFactory.createSubscriber();
+        sub.addConnection(cddl.getConnection());
+//        sub.subscribeServiceByName("my_service");
+        sub.subscribeServiceByName("Location");
+        sub.setSubscriberListener(new ISubscriberListener() {
+            @Override
+            public void onMessageArrived(Message message) {
+//                if (message.getServiceName().equals("my_service")) {
+//                    Log.d("_MAIN", "+++" + message.getSourceLocationLatitude());
+//                }
+                if (message.getServiceName().equals("Location")) {
+                    latitude = message.getSourceLocationLatitude();
+                    longitude = message.getSourceLocationLongitude();
                 }
                 Log.d("_MAIN", ">>>>>>>>>>>>>>>>>>>>>>>>>>>>" + message);
             }
         });
-
     }
+
+    private IConnectionListener connectionListenerExternal = new IConnectionListener() {
+        @Override
+        public void onConnectionEstablished() {
+            messageTextView.setText("Conexão estabelecida.");
+        }
+
+        @Override
+        public void onConnectionEstablishmentFailed() {
+            messageTextView.setText("Falha na conexão.");
+        }
+
+        @Override
+        public void onConnectionLost() {
+            messageTextView.setText("Conexão perdida.");
+        }
+
+        @Override
+        public void onDisconnectedNormally() {
+            messageTextView.setText("Uma disconexão normal ocorreu.");
+        }
+
+    };
 
     private IConnectionListener connectionListener = new IConnectionListener() {
         @Override
@@ -138,7 +209,7 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            publishMessage();
+            publishExternal(latitude, longitude);
         }
     };
 
@@ -167,7 +238,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 Intent intent = new Intent(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
                 intent.addCategory("android.intent.category.DEFAULT");
-                intent.setData(Uri.parse(String.format("package:%s",getApplicationContext().getPackageName())));
+                intent.setData(Uri.parse(String.format("package:%s", getApplicationContext().getPackageName())));
 //                intent.putExtra(caFileName, caFileName);
 //                intent.putExtra(certFileName, certFileName);
 //                intent.putExtra(caAlias, caAlias);
@@ -183,16 +254,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void setCertificates(String caFileName, String certFileName, String caAlias){
-        if(!checkPermission()){
+    private void setCertificates(String caFileName, String certFileName, String caAlias) {
+        if (!checkPermission()) {
             requestPermission();
-        }else{
-            try{
+        } else {
+            try {
                 SecurityService securityService = new SecurityService(getApplicationContext());
                 securityService.setCaCertificate(caFileName, caAlias);
                 securityService.setCertificate(certFileName);
-            }
-            catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
